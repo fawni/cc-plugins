30c09a6480a85d136e702d9b8129f89b59815bb6
obscurity <z@x4.pm>
2022-01-04 20:16:55 +0200

    stay away from greentext-less lines
    on the expanse of muh leet code, i blame discord

--- Greentext/plugin.js
+++ Greentext/plugin.js
@@ -1,12 +1,12 @@
-import css from "./style.sass";
-import settings from "./settings";
+import css from "./style.sass"
+import settings from "./settings"
 
-import { after } from '@cumcord/patcher';
-import { find } from "@cumcord/modules/webpack";
-import { React } from '@cumcord/modules/common';
-import { createElement } from "react";
+import { after } from "@cumcord/patcher"
+import { find } from "@cumcord/modules/webpack"
+import { React } from "@cumcord/modules/common"
+import { createElement } from "react"
 
-let unpatch, uninjectCss;
+let unpatch, uninjectCss
 const msg = find(m => m.type?.displayName === "MessageContent")
 
 export default ({ persist }) => {
@@ -14,28 +14,29 @@ export default ({ persist }) => {
         onLoad() {
             if (persist.ghost.text == undefined) persist.store.text = true
             if (persist.ghost.quote == undefined) persist.store.quote = true
-            uninjectCss = css();
+            uninjectCss = css()
             unpatch = after("type", msg, (_, res) => {
                 const message = res.props?.children[0]
                 for (const text in message) {
                     if (persist.ghost.text && typeof message[text] === "string") {
                         const lines = message[text].split("\n")
+                        let changed = false
                         for (const line in lines) {
                             if (typeof lines[line] !== "string" || !lines[line].startsWith(">")) continue
-                            lines[line] = React.createElement("div", { className: "greentext" }, lines[line]);
+                            lines[line] = React.createElement("div", { className: "greentext" }, lines[line])
+                            changed = true
                         }
-                        message[text] = lines
+                        if (changed === true) message[text] = lines
                     } else if (persist.ghost.quote && message[text]?.props?.className?.includes("blockquoteContainer")) {
-                        message[text].props.className += " greentext";
+                        message[text].props.className += " greentext"
                     }
                 }
-
                 return res
-            });
+            })
         },
         onUnload() {
-            unpatch();
-            uninjectCss();
+            unpatch()
+            uninjectCss()
         },
         settings: createElement(settings, { persist })
     }
