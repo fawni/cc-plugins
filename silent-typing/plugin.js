48a5748a0dd6b40cc41bbf19f12a177380bb6d2b
x6r <x6r@users.noreply.github.com>
2022-06-21 23:15:17 +0000

    deploy: 701238f751ba0b66b50c0e39478479753a0097dd

---
+++ silent-typing/plugin.js
@@ -0,0 +1,1 @@
+(function(t,r){"use strict";let n,o=r.findByProps("startTyping");var a=()=>({onLoad(){n=t.instead("startTyping",o,()=>{})},onUnload(){n()}});return a})(cumcord.patcher,cumcord.modules.webpack);
