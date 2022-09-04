9967df2b722919296f0cb4f065184d9b88f9094e
obscurity <z@x4.pm>
2022-09-04 20:08:45 +0200

    chore: use for in

--- randomize-upload/plugin.js
+++ randomize-upload/plugin.js
@@ -24,7 +24,7 @@ export default () => {
 		onLoad() {
 			unpatch = before("uploadFiles", uploadModule, (res) => {
 				let files = res[0]?.uploads;
-				for (let i = 0; i < files.length; i++) {
+				for (let i in files) {
 					let file = files[i];
 					file.filename = `${rand()}.${file.filename.slice(
 						(Math.max(0, file.filename.lastIndexOf(".")) || Infinity) + 1
